f04842fb5b29701c89196e6373a424b7d08896e3
Rahul Prem <117741770+rp097@users.noreply.github.com>
2024-02-07 16:45:07 +1100

    Time: 732 ms (15.80%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -5,4 +5,4 @@ union all
 select accepter_id id from RequestAccepted)
 
 
-select id, count(*) num  from base group by 1 order by 2 desc limit 1
+select id, count(*) num  from base group by id order by count(*) desc limit 1
