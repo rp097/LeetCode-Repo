9033f48f0fd338b37c00c891020610ec7731092b
Rahul Prem <117741770+rp097@users.noreply.github.com>
2024-02-15 14:16:24 +1100

    Time: 113 ms (90.59%) | Memory: 13.2 MB (92.96%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -19,6 +19,18 @@ class Solution(object):
                 count = count - 1
 
         return candidate 
+        # c = [0]*len(nums)
+        # for i in range(0,len(nums),1):
+        #     #nums[i] outer loop
+        #     #initialise c[i] to 0
+        #     c[i] = 0
+        #     for j in range(0,len(nums),1):
+        #         #to count number of occurences of nums[i]
+        #         if nums[i] == nums[j]:
+        #             c[i] = c[i]+1
         
+        # for i in range(0,len(nums),1):
+        #     if c[i] > len(nums)/2:
+        #         return nums[i]      
         
         
