02e103deaf7e6f3dccf1c3c44841b4f09c5c07d5
Rahul Prem <117741770+rp097@users.noreply.github.com>
2024-02-15 14:30:14 +1100

    Time: 119 ms (70.39%) | Memory: 13.2 MB (83.35%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,21 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #Use Boyer Moore Majority Voting algorithm
+        #Method 1 - Use Boyer Moore Majority Voting algorithm
         #nums = [2,2,1,1,1,2,2]
 
-        candidate = 0
-        count = 0
-        for n in nums:
-            if count==0:
-                candidate = n
-                count = count+1
-            elif candidate == n:
-                count = count+1
-            elif candidate != n:
-                count = count - 1
+        # candidate = 0
+        # count = 0
+        # for n in nums:
+        #     if count==0:
+        #         candidate = n
+        #         count = count+1
+        #     elif candidate == n:
+        #         count = count+1
+        #     elif candidate != n:
+        #         count = count - 1
 
-        return candidate 
+        # return candidate 
+
+        #Method 2 - using for loops. One test case fails due to time limit exceeding.
         # c = [0]*len(nums)
         # for i in range(0,len(nums),1):
         #     #nums[i] outer loop
@@ -32,5 +34,13 @@ class Solution(object):
         # for i in range(0,len(nums),1):
         #     if c[i] > len(nums)/2:
         #         return nums[i]      
+
+        #Method 3 - using list methods to reduce time complexity
+        d_nums = set(nums) #stores all distinct numbers in d_nums
+
+        for x in d_nums:
+            c = nums.count(x)
+            if c > len(nums)/2:
+                return x
         
         
